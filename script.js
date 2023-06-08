68332822cb8efea2f793760ae68091571a1c7f4e
Svahnen-Perific <jonny.svahn@perific.com>
2023-06-08 14:17:40 +0200

    Create dynamic functions

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let getData = () => {
-    fetch("https://pushdata.io/cranky_skunk5@example.com/person1")
+let getData = (email, serie) => {
+    fetch("https://pushdata.io/" + email + "/" + serie)
         .then(res => res.json())
         .then(data => console.log(data));
 }
 
-let postData = (value) => {
-    var url = "https://pushdata.io/cranky_skunk5@example.com/person1/" + value
+let postData = (email, serie, value) => {
+    var url = "https://pushdata.io/" + email + "/" + serie + "/" + value
+    console.log(url)
     var xhr = new XMLHttpRequest()
     xhr.open("POST", url, true)
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
